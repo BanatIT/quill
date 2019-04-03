a7227cd5b61384348663e1becf9f42cc54d631fa
Timo Bejan <timo.bejan.ext@iqser.com>
2019-04-03 18:45:55 +0300

    crud for admin user

--- config/admin.js
+++ config/admin.js
@@ -5,20 +5,33 @@ ADMIN_PASSWORD = process.env.ADMIN_PASS;
 var User = require('../app/server/models/User');
 
 // If there is already a user
-User
-  .findOne({
+User.findOne({
     email: ADMIN_EMAIL
-  })
-  .exec(function(err, user){
-      var u = new User();
-      u.email = ADMIN_EMAIL;
-      u.ticketId = 'admin';
-      u.password = User.generateHash(ADMIN_PASSWORD);
-      u.admin = true;
-      u.verified = true;
-      u.save(function(err){
-        if (err){
-          console.log(err);
-        }
-      });
-  });
+}).exec(function (err, user) {
+    if (!user) {
+        user = new User();
+        setProperties(user);
+        u.save(function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    } else {
+        setProperties(user);
+        u.update(function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+});
+
+
+function setProperties(user) {
+    user.email = ADMIN_EMAIL;
+    user.ticketId = 'admin';
+    user.password = User.generateHash(ADMIN_PASSWORD);
+    user.admin = true;
+    user.verified = true;
+}
+}
