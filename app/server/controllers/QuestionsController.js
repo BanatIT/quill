8261bb9cb0d0b9cabfc693ce2b40f25ffaf93b48
Timo Bejan <timo.bejan.ext@iqser.com>
2019-04-05 17:34:00 +0300

    stuff

--- app/server/controllers/QuestionsController.js
+++ app/server/controllers/QuestionsController.js
@@ -11,7 +11,7 @@ QuestionsController.create = function (userId, value, callback) {
     Question.findOneAndUpdate({}, newQuestion, {upsert: true}, function (err, resp) {
         console.log('Created question:  ', resp, err);
         if (callback) {
-            callback();
+            callback(err, resp);
         }
     });
 };
@@ -20,26 +20,26 @@ QuestionsController.mark = function (questionId, callback) {
     Question.findOneAndUpdate({_id: questionId}, {answered: 'YES'}, {upsert: false}, function (err, resp) {
         console.log('Responded question:  ', resp, err);
         if (callback) {
-            callback();
+            callback(err, resp);
         }
     });
 };
 
 QuestionsController.findAll = function (callback) {
     Question.find({}, function (err, questions) {
-        callback(questions);
+        callback(err, questions);
     });
 };
 
 QuestionsController.findOpen = function (callback) {
     Question.find({answered: 'NO'}, function (err, questions) {
-        callback(questions);
+        callback(err, questions);
     });
 };
 
 QuestionsController.findMine = function (userId, callback) {
     Question.find({userId: userId}, function (err, questions) {
-        callback(questions);
+        callback(err, questions);
     });
 };
 
